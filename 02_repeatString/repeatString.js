520d527dbbdd028eae3657bb426e29223d73bc97
jcampbell <jcampbell57@gmail.com>
2022-06-04 10:27:49 -0700

    solved repeatString javascript exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let message;
+let j;
 
+const repeatString = function(message, j) {
+    let output = [];
+    if (j<0) {
+        return 'ERROR';
+    } else {
+        for (i=0; i<j; i++) {
+            output.push(message);
+        }
+        return output.join('');    
+    }
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
