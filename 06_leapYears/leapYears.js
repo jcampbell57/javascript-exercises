7fcfe9a37262985643d1f95d6c9a6910b39118fe
jcampbell <jcampbell57@gmail.com>
2022-06-04 18:23:46 -0700

    solved leapYears javascript exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYear = ''
+    if (year % 100 == 0 && year % 400 !== 0) {
+        return false;
+    } else if (year % 4 !== 0) {
+        return false;
+    } else if (year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 100 !== 0) {
+        return true;
+    } else {
+        return 'oops';
+    }
 };
 
 // Do not edit below this line
