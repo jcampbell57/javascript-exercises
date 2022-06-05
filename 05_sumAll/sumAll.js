4429c7b451613a885d1d30dd3ec18c1ec7145a7d
jcampbell <jcampbell57@gmail.com>
2022-06-04 17:46:21 -0700

    solved sumAll javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(bottom, top) {
+    let sum = 0;   
+    if (bottom < 0 || top < 0 || typeof bottom !== 'number' || typeof top !== 'number') {
+        return 'ERROR'
+    } else if (bottom < top) {
+        for (let i=bottom; i<=top; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        for (let i=top; i<=bottom; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
