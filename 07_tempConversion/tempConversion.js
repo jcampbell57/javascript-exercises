a604b4c62e5b9da0abc458ff10fb195b227b52e8
jcampbell <jcampbell57@gmail.com>
2022-06-04 20:29:16 -0700

    solved tempConversion javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(degreesF) {
+  celsius = (degreesF - 32) * (5/9);
+  output = Math.round(celsius * 10) / 10;
+  return output;
 };
 
-const ctof = function() {
-
+const ctof = function(degreesC) {
+  farh = degreesC * (9/5) + 32;
+  output = Math.round(farh * 10) / 10;
+  return output;
 };
 
 // Do not edit below this line
