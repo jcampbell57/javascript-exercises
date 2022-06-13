3c9cf3b144e11dd1cf22028fa5b0ea5e373c4a75
jcampbell <jcampbell57@gmail.com>
2022-06-13 09:21:43 -0700

    solved fibonacci exercise"

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 1) {
+        return 'OOPS';
+    } else {
+        let n1 = 1, n2 = 1, nextnum;
+        for (let i=1; i<input; i++) {
+          nextTerm = n1 + n2
+          n1 = n2
+          n2 = nextTerm
+        }
+      return n1;
+    }
 };
 
 // Do not edit below this line
