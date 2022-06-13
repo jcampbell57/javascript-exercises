1e7da5602da1dd520c2dbbae6ab2d230836dd5b9
jcampbell <jcampbell57@gmail.com>
2022-06-12 17:14:57 -0700

    solved calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0
+  for (i=0; i < arr.length; i++) {
+    sum += arr[i];
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let answer = 1;
+  for (i=0; i < arr.length; i++) {
+    answer *= arr[i];
+  };
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let answer = 1;
+  if (input === 0) {
+    return answer
+  } else {
+    for (i=input-1; i>0; i--) {
+      input *= i;
+    }
+    return input
+  }
 };
 
 // Do not edit below this line
