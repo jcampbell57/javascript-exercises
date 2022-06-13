1c2172cb71b352f84916738ad0f440083e0ead73
jcampbell <jcampbell57@gmail.com>
2022-06-13 11:17:44 -0700

    completed findTheOldest javascript tutorial

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) { 
+    people.filter(function(person) {
+      if (person.yearOfDeath == undefined) {
+        person.yearOfDeath = (new Date()).getFullYear();
+      }
+    })
+    const sorted = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return sorted[0]
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
