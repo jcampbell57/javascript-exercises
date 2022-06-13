bfb0f585f1cd1a7b76061c08a012bc5722edbd7a
jcampbell <jcampbell57@gmail.com>
2022-06-12 20:35:11 -0700

    solved palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (a) {
+    let frontwards = a
+      .toLowerCase()
+      .replace(/[^\p{L}\s]/gu,"")
+      .replace(/\s+/g,'');
+    let splitFront = frontwards.split('');
+    let backwards = splitFront.reverse().join('');
+    return (frontwards === backwards) ? true : false;
+  }
+  
 // Do not edit below this line
 module.exports = palindromes;
